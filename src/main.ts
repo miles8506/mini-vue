af00dd2c21aa9ac881e4027d089666b2a5b63f47
miles <a09112332@gmail.com>
2024-08-03 18:08:31 +0800

    chore: test

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
-import { h, mount } from './utils/render'
+import { h, mount, patch } from './utils/render'
 
-const vNodes = h('div', { class: 'foo' }, [
+const vNodes = h('div', { class: 'foo', id: 'aaa' }, [
   h('h2', null, '當前count:'),
   h('button', { onClick: () => { console.log('bar'); } }, 'click')
 ])
 
 mount(vNodes, document.querySelector('#app'))
+
+const newNode = h('div', { class: 'bar' }, [h('h2', null, '當前count:'), h('div', null, 'qqq')])
+
+setTimeout(() => {
+  patch(vNodes, newNode)
+}, 2000);
