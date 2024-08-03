90641ac690a7d3e942aa27610614138b515c581f
miles <a09112332@gmail.com>
2024-08-03 16:55:01 +0800

    feat: patch tag&props

--- src/utils/render.ts
+++ src/utils/render.ts
@@ -26,9 +26,11 @@ export function mount(vNode: VNode, container: HTMLElement) {
     Object.keys(vNode.props).forEach(key => {
       const val = vNode.props[key]
 
-      key.startsWith('on') ? 
-        el.addEventListener(key.substring(2).toLowerCase(), val as (() => {})) :
+      if (key.startsWith('on')) {
+        el.addEventListener(key.substring(2).toLowerCase(), val as (() => {}))
+      } else {
         el.setAttribute(key, val as string)
+      }
     })
   }
 
@@ -44,4 +46,39 @@ export function mount(vNode: VNode, container: HTMLElement) {
   }
 
   container.appendChild(el)
+}
+
+export function patch(n1: VNode, n2: VNode) {
+  const el = n2.el = n1.el
+  // tag
+  if (n1.tag !== n2.tag) {
+    const parentEl = n1.el.parentElement
+    parentEl.removeChild(n1.el)
+    mount(n2, parentEl)
+  }
+
+  // props
+  const oldProps = n1.props ?? {}
+  const newProps = n2.props ?? {}
+  for (const key in oldProps) {
+    if (oldProps[key] !== newProps[key]) {
+      if (key.startsWith('on')) {
+        el.addEventListener(key.substring(2).toLowerCase(), newProps[key] as (() => {}))
+      } else {
+        el.setAttribute(key, newProps[key] as string)
+      }
+    }
+  }
+
+  for (const key in oldProps) {
+    if (!(key in newProps)) {
+      if (key.startsWith('on')) {
+        el.removeEventListener(key.substring(2).toLowerCase(), newProps[key] as (() => {}))
+      } else {
+        el.removeAttribute(key)
+      }
+    }
+  }
+
+  // children
 }
